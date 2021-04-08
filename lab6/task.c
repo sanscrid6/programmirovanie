081bba97c212841634a5690ce007349e984bce37
sanscrid6 <65036584+sanscrid6@users.noreply.github.com>
2021-04-08 12:56:07 +0300

    Update task.c

--- lab6/task.c
+++ lab6/task.c
@@ -7,20 +7,10 @@
 int main()
 {
     int length = 0, sum = 0, i = 0, check;
-    unsigned int size;
     struct Node* root = NULL;
-    srand(time(0));
-    size = 10 + rand()%30;
-    for(i = 0; i<size;i++)
-    {
-        check = addNode(&root, rand()%35);
-        if(check == 1)
-        {
-            printf("Malloc error");
-            return 1;
-        }
-    }
-    sumOfLongestBranch(&root, 0, &length, &sum);
+    inputHandler(&root);
+    if(root == NULL) printf("Tree is empty\n");
+    else sumOfLongestBranch(&root, 0, &length, &sum);
     printf("Max length of branch is %i\nSum is %i", length, sum);
     freeTree(root);
     return 0;
