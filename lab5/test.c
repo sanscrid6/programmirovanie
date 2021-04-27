d2c8c9266505900699061be94f2b075066092f89
sanscrid6 <65036584+sanscrid6@users.noreply.github.com>
2021-04-27 11:51:04 +0300

    Update test.c

--- lab5/test.c
+++ lab5/test.c
@@ -7,16 +7,27 @@
 
 void test()
 {
-    struct Node* head = NULL;
-    struct Node* tail = NULL;
-    assert(push_back(&head, &tail, 1) == 0);
-    assert(push_back(&head, &tail, 2) == 0);
-    assert(push_back(&head, &tail, 3) == 0);
-    assert(push_back(&head, &tail, 4) == 0);
+    int i = 0;
+    struct Node *head1 = NULL, *head2 = NULL, *head3 = NULL;
+    struct Node *tail1 = NULL, *tail2 = NULL, *tail3 = NULL;
+    assert(push_back(&head1, &tail1, 1) == 0);
+    assert(push_back(&head1, &tail1, 3) == 0);
+    assert(push_back(&head1, &tail1, 5) == 0);
+    assert(push_back(&head2, &tail2, 2) == 0);
+    assert(push_back(&head2, &tail2, 3) == 0);
+    assert(push_back(&head2, &tail2, 4) == 0);
+    delElements(&head1, &head2, &head3, &tail3);
+    for(i = 1 ; i<=5; i++)
+    {
+        assert(head3->value == i );
+        head3 = head3->next;
+    }
     assert(isEqual(1,2) == 0);
     assert(isEqual(1,1) == 1);
-    assert(countSize(&head) == 4);
-    freeList(&head);
+    assert(countSize(&head1) == 3);
+    freeList(&head1);
+    freeList(&head2);
+    freeList(&head3);
 }
 
 int main()
